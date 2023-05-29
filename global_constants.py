89810fb292c52dba04b9630e44b06026eb61a751
Marco Conciatori <conciatori.marco91@gmail.com>
2023-05-29 20:51:13 +0900

    added config and constants files

---
+++ global_constants.py
@@ -0,0 +1,20 @@
+IMAGE_DIMENSIONS = (50, 50)
+TREE_CATEGORIES_GIAPANESE = ('Buna', 'Hauchiwakaede', 'Inutsuge', 'Koshiabura',
+                             'Kyaraboku', 'Matsu', 'Minekaede', 'Mizuki', 'Mizunara', 'Nanakamado')
+TREE_INFORMATION = {
+    0: {
+        'japanese_reading': 'buna',
+        'scientific_name': 'fagus crenata',
+        'common_name': 'beech',
+        'japanese_name': 'ブナ',
+        'type': 'deciduous',
+    },
+    1: {
+        'japanese_reading': '',
+        'scientific_name': '',
+        'common_name': '',
+        'japanese_name': '',
+        'type': '',
+    },
+
+}
