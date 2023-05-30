a7da093a92830996b88202e75a33f39b48aa5855
Marco Conciatori <conciatori.marco91@gmail.com>
2023-05-30 17:37:38 +0900

    main updated

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import torch
-x = torch.rand(5, 3)
-print(x)
-print(torch.cuda.is_available())
+import config
+import global_constants
+from data_preprocessing import data_loading
+
+
+verbose = config.VERBOSE
+img_list = data_loading.load_img(global_constants.DATA_PATH, verbose=verbose)
