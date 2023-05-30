895c1e95168f58775c83c71b903c9266cddae6cb
Marco Conciatori <conciatori.marco91@gmail.com>
2023-05-30 17:36:52 +0900

    added image loading

---
+++ data_preprocessing/data_loading.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+
+def load_img(data_path, verbose: int = 0) -> list:
+    pure_path = Path(data_path)
+    assert pure_path.exists(), f'Path {data_path} does not exist.'
+    assert pure_path.is_dir(), f'Path {data_path} is not a directory.'
+
+    img_list = []
+    for img_path in pure_path.iterdir():
+        assert img_path.is_file(), f'Path {img_path} is not a file.'
+        img_list.append(img_path)
+
+    if verbose >= 2:
+        print(f'Loaded {len(img_list)} images from {data_path}.')
+
+    return img_list
