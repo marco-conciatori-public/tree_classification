655f7a5eb6bc29b4e563f93cdb72ca49e7d01d6a
Marco Conciatori <conciatori.marco91@gmail.com>
2023-06-02 16:37:58 +0900

    refactoring

--- data_preprocessing/data_loading.py
+++ data_preprocessing/data_loading.py
@@ -1,10 +1,10 @@
 from pathlib import Path
 
 
-def load_img(data_path, verbose: int = 0) -> list:
+def load_img(data_path: str, verbose: int = 0) -> list:
     pure_path = Path(data_path)
-    assert pure_path.exists(), f'Path {data_path} does not exist.'
-    assert pure_path.is_dir(), f'Path {data_path} is not a directory.'
+    assert pure_path.exists(), f'Path "{data_path}" does not exist.'
+    assert pure_path.is_dir(), f'Path "{data_path}" is not a directory.'
 
     img_list = []
     for img_path in pure_path.iterdir():
@@ -12,6 +12,6 @@ def load_img(data_path, verbose: int = 0) -> list:
             img_list.append(img_path)
 
     if verbose >= 2:
-        print(f'Loaded {len(img_list)} images from {data_path}.')
+        print(f'Loaded {len(img_list)} images from "{data_path}".')
 
     return img_list
