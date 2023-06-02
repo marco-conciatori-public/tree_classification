91599d46d144e736d934f41d22c3df58bd20244b
Marco Conciatori <conciatori.marco91@gmail.com>
2023-06-02 16:39:04 +0900

    added function to resixe img, renamed file

---
+++ data_preprocessing/standardize_img.py
@@ -0,0 +1,47 @@
+import cv2
+
+import global_constants
+
+
+def get_min_dimensions(img_list: list) -> (int, int):
+    min_width = 1000000
+    min_height = 1000000
+    for img_path in img_list:
+        img_path = str(img_path)
+        try:
+            img = cv2.imread(img_path)
+        except Exception as e:
+            print(f'ERROR: could not load image {img_path}. Exception: {e}')
+            continue
+        try:
+            width, height = img.shape[:2]
+            if width < min_width:
+                min_width = width
+            if height < min_height:
+                min_height = height
+        except Exception as e:
+            print(f'ERROR: could not get shape of image {img_path}. Exception: {e}')
+            continue
+    return min_width, min_height
+
+
+def resize_img(img_path, min_width: int, min_height: int) -> None:
+    img_path = str(img_path)
+    try:
+        img = cv2.imread(img_path)
+    except Exception as e:
+        print(f'ERROR: could not load image {img_path}. Exception: {e}')
+        return
+    try:
+        img = cv2.resize(img, (min_width, min_height))
+    except Exception as e:
+        print(f'ERROR: could not resize image {img_path}. Exception: {e}')
+        return
+    # the [:-1] is used to remove due "\", that causes problems
+    img_path = img_path.replace(global_constants.DATA_PATH[:-1], global_constants.PREPROCESSED_DATA_PATH[:-1])
+    try:
+        cv2.imwrite(img_path, img)
+    except Exception as e:
+        print(f'ERROR: could not save image {img_path}. Exception: {e}')
+        return
+
--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import config
 import global_constants
-from data_preprocessing import data_loading
+from data_preprocessing import data_loading, standardize_img
 
 
 verbose = config.VERBOSE
 img_list = data_loading.load_img(global_constants.DATA_PATH, verbose=verbose)
+# get min width and height separately
+min_width, min_height = standardize_img.get_min_dimensions(img_list)
+if verbose >= 2:
+    print(f'Minimum width: {min_width}, minimum height: {min_height}.')
+
+for img_path in img_list:
+    # resize images to the smallest width and height found in the dataset
+    standardize_img.resize_img(img_path, min_width, min_height)
