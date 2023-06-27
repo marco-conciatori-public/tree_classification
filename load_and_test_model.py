c8b4340ab77c507c364392a0f68042962a8d8f72
Marco Conciatori <conciatori.marco91@gmail.com>
2023-06-27 11:18:54 +0900

    added script for loading ant testing model

---
+++ load_and_test_model.py
@@ -0,0 +1,76 @@
+import torch
+import numpy as np
+
+import config
+import utils
+import global_constants
+from models import model_utils
+from data_preprocessing import get_ready_data
+
+
+cpu = torch.device('cpu')
+
+# PARAMETERS
+verbose = config.VERBOSE
+model_id = 3
+partial_name = ''
+
+model_path, info_path = utils.get_path_by_id(
+    partial_name=partial_name,
+    model_id=model_id,
+    folder_path=global_constants.MODEL_OUTPUT_DIR,
+)
+
+loaded_model, meta_data = model_utils.load_model(
+    model_path=model_path,
+    device=cpu,
+    training_mode=False,
+    meta_data_path=info_path,
+    verbose=verbose,
+)
+
+# observation = np.random.random_sample(size=(7, 6))
+# observation = torch.tensor(observation).to(device)
+# print('Observation/s:')
+# print(observation)
+# print(f'dimensions: {observation.shape}')
+train_dl, val_dl, test_dl, img_shape = get_ready_data.get_data(
+    shuffle=False,
+    batch_size=1,
+    train_val_test_proportions=config.TRAIN_VAL_TEST_PROPORTIONS,
+    tolerance=config.TOLERANCE,
+    augment_data=0,
+    verbose=verbose,
+)
+
+print(f'len(test_dl): {len(test_dl)}')
+
+# get loss function from string name
+loss_function = getattr(torch.nn, config.LOSS_FUNCTION_NAME)()
+
+test_loss = 0.0
+loss_counter = 0
+targets = []
+predictions = []
+with torch.set_grad_enabled(False):
+    for obs, target in test_dl:
+        prediction = loaded_model(obs)
+
+        loss = loss_function(prediction, target)
+        loss_counter += 1
+        test_loss += loss
+
+        # print('----------------')
+        # print(f'target: {target}')
+        # print(f'prediction: {prediction}')
+        targets.append(target)
+        predictions.append(prediction)
+
+print(f'len(targets): {len(targets)}')
+print(f'len(predictions): {len(predictions)}')
+targets = np.array(targets)
+predictions = np.array(predictions)
+
+test_loss = test_loss / loss_counter
+print(f'loss_counter: {loss_counter}')
+print(f'test_loss: {test_loss}')
