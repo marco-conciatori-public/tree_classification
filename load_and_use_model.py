1655ed688940b318e74601757041c7aab43ddcaf
Marco Conciatori <conciatori.marco91@gmail.com>
2023-06-27 15:46:36 +0900

    imported images in use_model script

--- load_and_use_model.py
+++ load_and_use_model.py
@@ -1,10 +1,11 @@
 import torch
+import torchvision.transforms.functional as tf
 
-import config
 import utils
+import config
 import global_constants
-from models import model_utils, evaluation
-from data_preprocessing import get_ready_data
+from models import model_utils
+from data_preprocessing import data_loading
 
 
 cpu = torch.device('cpu')
@@ -13,8 +14,15 @@ cpu = torch.device('cpu')
 verbose = 2
 model_id = 0
 partial_name = ''
+jump = 50
 
-img_list = []
+img_list, tag_list = data_loading.load_data(data_path=global_constants.PREPROCESSED_DATA_PATH, verbose=verbose)
+print(f'img_list length: {len(img_list)}')
+
+tree_class_counter = [0, 0, 0, 0, 0]
+for tag in tag_list:
+    tree_class_counter[tag] += 1
+print(f'tree_class_counter: {tree_class_counter}')
 
 model_path, info_path = utils.get_path_by_id(
     partial_name=partial_name,
@@ -39,9 +47,17 @@ loss_counter = 0
 targets = []
 predictions = []
 with torch.set_grad_enabled(False):
-    for img in img_list:
-        tensor_img = torch.tensor(img).to(cpu)
+    for img_index in range(len(img_list)):
+        if img_index % jump != 0:
+            continue
+        print(f'img_index: {img_index}')
+        img = img_list[img_index]
+        tensor_img = tf.to_tensor(img)
+        tensor_img = tensor_img.unsqueeze(0)
         prediction = loaded_model(tensor_img)
+        prediction = torch.nn.Softmax(dim=1)(prediction)
+        prediction = prediction.squeeze(0)
+        prediction = prediction.numpy()
 
         print('-------------------')
         for tree_class in range(len(prediction)):
