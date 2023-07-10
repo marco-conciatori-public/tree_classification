cd172d0bafff3445db4ce47877b45c4bca772cba
MorichanPC <marco.conciatori@unipr.it>
2023-07-10 10:35:06 +0900

    showing original image

--- load_and_use_model.py
+++ load_and_use_model.py
@@ -6,7 +6,7 @@ import utils
 import config
 import global_constants
 from models import model_utils
-from data_preprocessing import get_ready_data
+from data_preprocessing import get_ready_data, data_loading
 
 
 cpu = torch.device('cpu')
@@ -32,7 +32,7 @@ loaded_model, custom_transforms, meta_data = model_utils.load_model(
 )
 
 train_dl, val_dl, test_dl, img_shape = get_ready_data.get_data(
-    shuffle=config.SHUFFLE,
+    shuffle=False,
     batch_size=1,
     train_val_test_proportions=config.TRAIN_VAL_TEST_PROPORTIONS,
     # standard_img_dim=config.IMG_DIM,
@@ -41,6 +41,13 @@ train_dl, val_dl, test_dl, img_shape = get_ready_data.get_data(
     augment_data=0,
     verbose=verbose,
 )
+
+step_2_img_list, _ = data_loading.load_data(
+            data_path=global_constants.STEP_2_DATA_PATH,
+            verbose=0,
+        )
+print(f'step_2_img_list length: {len(step_2_img_list)}')
+
 img_list = []
 tag_list = []
 for batch in train_dl:
@@ -57,9 +64,12 @@ for batch in test_dl:
     tag_list.append(target_batch.squeeze(0).item())
 print(f'img_list length: {len(img_list)}')
 # shortened_img_list = [img_list[0]]
+# shortened_step_2_img_list = [step_2_img_list[0]]
 shortened_img_list = [img_list[i] for i in range(0, len(img_list), jump)]
+shortened_step_2_img_list = [step_2_img_list[i] for i in range(0, len(img_list), jump)]
 print(f'shortened_img_list length: {len(shortened_img_list)}')
 shortened_tag_list = [tag_list[i] for i in range(0, len(tag_list), jump)]
+print(f'shortened_step_2_img_list length: {len(shortened_step_2_img_list)}')
 
 # get loss function from string name
 loss_function = getattr(torch.nn, config.LOSS_FUNCTION_NAME)()
@@ -83,8 +93,9 @@ with torch.set_grad_enabled(False):
                 print(f' - {global_constants.TREE_INFORMATION[tree_class]["japanese_reading"]}: '
                       f'{round(prediction[tree_class] * 100, max(global_constants.MAX_DECIMAL_PLACES - 2, 0))}')
 
+        img = shortened_step_2_img_list[img_index]
         # show image
-        img = img.squeeze(0).numpy().transpose(1, 2, 0)
+        print(img.shape)
         cv2.imshow(
             winname=global_constants.TREE_INFORMATION[shortened_tag_list[img_index]]["japanese_reading"].upper(),
             mat=img,
