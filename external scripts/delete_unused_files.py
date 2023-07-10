7df288b05272e459693e10c735c5156afa6bebbd
Marco Conciatori <conciatori.marco91@gmail.com>
2023-07-10 13:45:17 +0900

    refactoring

--- external scripts/delete_unused_files.py
+++ external scripts/delete_unused_files.py
@@ -1,18 +1,16 @@
 from pathlib import Path
 import cv2
 
-
 import global_constants
 
-# DELETE FILES BY EXTENSION
 original_data_path = global_constants.DATA_PATH + 'original_data/'
 pure_path = Path(global_constants.ONE_LEVEL_UP + original_data_path)
 print(f'pure_path: {pure_path}')
 assert pure_path.exists(), f'Path {original_data_path} does not exist'
 tif_list = []
+# correct wrong 's1' to 's2' naming for Minekaede_s2 folder
+# also correct wrong '＿' to '_' in file names
 for dir_path in pure_path.iterdir():
-    # correct wrong 's1' to 's2' naming for Minekaede_s2 folder
-    # also correct wrong '＿' to '_' in file names
     if dir_path.is_dir():
         if 'Minekaede_s2' in str(dir_path):
             for file_path in dir_path.iterdir():
@@ -29,11 +27,11 @@ for dir_path in pure_path.iterdir():
         folder_list = list(dir_path.glob('*.TIF'))
         tif_list.extend(folder_list)
 
-# save images to the step_1_data folder
+# save images to the STEP_1_DATA_PATH folder
 save_folder_path = Path(global_constants.ONE_LEVEL_UP + global_constants.STEP_1_DATA_PATH)
 if not save_folder_path.exists():
     save_folder_path.mkdir(parents=False)
-print(f'save_folder_path: {save_folder_path}')
+# print(f'save_folder_path: {save_folder_path}')
 for tif_path in tif_list:
     img_new_path = str(save_folder_path) + '/' + tif_path.name
     # print(f'img_new_path: {save_path}')
