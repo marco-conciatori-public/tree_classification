eec33497bdb79eef7a1972e93d51bbc05dce9420
Marco Conciatori <conciatori.marco91@gmail.com>
2023-07-12 11:45:44 +0900

    if training stopped, save best model so far

--- models/training.py
+++ models/training.py
@@ -48,53 +48,17 @@ def train(model: torch.nn.Module,
     history['metrics']['train'] = {}
     history['metrics']['validation'] = {}
 
-    # loop over the dataset 'epochs' times
-    best_model_weights = None
-    min_valid_loss = np.inf
-    batch_size = 0
-    for epoch in range(epochs):
-        batch_counter = 0
-        # TRAINING
-        training_loss = 0.0
-        model.train()
-        for batch in training_data:
-            observation_batch, target_batch = batch
-
-            if batch_counter == 0:
-                batch_size = len(target_batch)
-
-            if not target_batch.shape[0] == batch_size:
-                continue
-
-            # Potentially transfer batch to GPU
-            if observation_batch.device != device:
-                observation_batch = observation_batch.to(device)
-                target_batch = target_batch.to(device)
-            # print(f'observation_batch.shape: {observation_batch.shape}')
-            # print(f'target_batch.shape: {target_batch.shape}')
-            # print(f'target_batch: {target_batch}')
-
-            prediction_batch = model(observation_batch)
-            loss = loss_function(prediction_batch, target_batch)
-
-            # update metrics
-            for metric in training_metrics.values():
-                metric.update(prediction_batch.cpu(), target_batch.cpu())
-
-            optimizer.zero_grad()
-            loss.backward()
-            # update network parameters
-            optimizer.step()
-
-            training_loss += loss.item()
-            batch_counter += 1
-
-        # VALIDATION
-        validation_loss = 0.0
-        batch_counter = 0
-        model.eval()
-        with torch.set_grad_enabled(False):
-            for batch in validation_data:
+    try:
+        # loop over the dataset 'epochs' times
+        best_model_weights = None
+        min_valid_loss = np.inf
+        batch_size = 0
+        for epoch in range(epochs):
+            batch_counter = 0
+            # TRAINING
+            training_loss = 0.0
+            model.train()
+            for batch in training_data:
                 observation_batch, target_batch = batch
 
                 if batch_counter == 0:
@@ -107,82 +71,142 @@ def train(model: torch.nn.Module,
                 if observation_batch.device != device:
                     observation_batch = observation_batch.to(device)
                     target_batch = target_batch.to(device)
+                # print(f'observation_batch.shape: {observation_batch.shape}')
+                # print(f'target_batch.shape: {target_batch.shape}')
+                # print(f'target_batch: {target_batch}')
 
                 prediction_batch = model(observation_batch)
                 loss = loss_function(prediction_batch, target_batch)
 
                 # update metrics
-                for metric in validation_metrics.values():
+                for metric in training_metrics.values():
                     metric.update(prediction_batch.cpu(), target_batch.cpu())
 
-                validation_loss += loss.item()
+                optimizer.zero_grad()
+                loss.backward()
+                # update network parameters
+                optimizer.step()
+
+                training_loss += loss.item()
                 batch_counter += 1
 
-        training_loss = training_loss / len(training_data)
-        validation_loss = validation_loss / len(validation_data)
+            # VALIDATION
+            validation_loss = 0.0
+            batch_counter = 0
+            model.eval()
+            with torch.set_grad_enabled(False):
+                for batch in validation_data:
+                    observation_batch, target_batch = batch
 
-        if verbose >= 2:
-            print(f'Epoch {epoch + 1}')
+                    if batch_counter == 0:
+                        batch_size = len(target_batch)
 
-            # print metrics results for the current epoch
-            print('Training results:')
-            print(f'\tTraining Loss: {training_loss}')
-            for metric_name in training_metrics:
-                metric = training_metrics[metric_name]
-                result = metric.compute()
-                print(f'\t{metric_name}: {round(result.item(), global_constants.MAX_DECIMAL_PLACES)}')
+                    if not target_batch.shape[0] == batch_size:
+                        continue
 
-            print('Validation results:')
-            print(f'\tValidation Loss: {validation_loss}')
-            for metric_name in validation_metrics:
-                metric = validation_metrics[metric_name]
-                result = metric.compute()
-                print(f'\t{metric_name}: {round(result.item(), global_constants.MAX_DECIMAL_PLACES)}')
+                    # Potentially transfer batch to GPU
+                    if observation_batch.device != device:
+                        observation_batch = observation_batch.to(device)
+                        target_batch = target_batch.to(device)
 
-        history['loss']['train'].append(training_loss)
-        history['loss']['validation'].append(validation_loss)
+                    prediction_batch = model(observation_batch)
+                    loss = loss_function(prediction_batch, target_batch)
 
-        if validation_loss < min_valid_loss:
+                    # update metrics
+                    for metric in validation_metrics.values():
+                        metric.update(prediction_batch.cpu(), target_batch.cpu())
+
+                    validation_loss += loss.item()
+                    batch_counter += 1
+
+            training_loss = training_loss / len(training_data)
+            validation_loss = validation_loss / len(validation_data)
+
             if verbose >= 2:
-                print(f' \t\t Validation loss decreased({min_valid_loss:.6f} '
-                      f'--> {validation_loss:.6f}) \t Saving the model')
+                print(f'Epoch {epoch + 1}')
 
-            min_valid_loss = validation_loss
-            # Saving model weights
-            best_model_weights = copy.deepcopy(model.state_dict())
+                # print metrics results for the current epoch
+                print('Training results:')
+                print(f'\tTraining Loss: {training_loss}')
+                for metric_name in training_metrics:
+                    metric = training_metrics[metric_name]
+                    result = metric.compute()
+                    print(f'\t{metric_name}: {round(result.item(), global_constants.MAX_DECIMAL_PLACES)}')
 
-    # reload the best model found
-    model.load_state_dict(best_model_weights)
+                print('Validation results:')
+                print(f'\tValidation Loss: {validation_loss}')
+                for metric_name in validation_metrics:
+                    metric = validation_metrics[metric_name]
+                    result = metric.compute()
+                    print(f'\t{metric_name}: {round(result.item(), global_constants.MAX_DECIMAL_PLACES)}')
 
-    for metric_name in training_metrics:
-        metric = training_metrics[metric_name]
-        history['metrics']['train'][metric_name] = metric.compute()
-    for metric_name in validation_metrics:
-        metric = validation_metrics[metric_name]
-        history['metrics']['validation'][metric_name] = metric.compute()
+            history['loss']['train'].append(training_loss)
+            history['loss']['validation'].append(validation_loss)
 
-    if verbose >= 1:
-        model_utils.print_formatted_results(
-            title='TRAINING RESULTS',
-            loss=history['loss']['train'][-1],
-            metrics=history['metrics']['train'],
-        )
+            if validation_loss < min_valid_loss:
+                if verbose >= 2:
+                    print(f' \t\t Validation loss decreased({min_valid_loss:.6f} '
+                          f'--> {validation_loss:.6f}) \t Saving the model')
 
-        model_utils.print_formatted_results(
-            title='VALIDATION RESULTS',
-            loss=history['loss']['validation'][-1],
-            metrics=history['metrics']['validation'],
-        )
+                min_valid_loss = validation_loss
+                # Saving model weights
+                best_model_weights = copy.deepcopy(model.state_dict())
+
+        # reload the best model found
+        model.load_state_dict(best_model_weights)
+
+        for metric_name in training_metrics:
+            metric = training_metrics[metric_name]
+            history['metrics']['train'][metric_name] = metric.compute()
+        for metric_name in validation_metrics:
+            metric = validation_metrics[metric_name]
+            history['metrics']['validation'][metric_name] = metric.compute()
+
+        if verbose >= 1:
+            model_utils.print_formatted_results(
+                title='TRAINING RESULTS',
+                loss=history['loss']['train'][-1],
+                metrics=history['metrics']['train'],
+            )
+
+            model_utils.print_formatted_results(
+                title='VALIDATION RESULTS',
+                loss=history['loss']['validation'][-1],
+                metrics=history['metrics']['validation'],
+            )
+
+    # if training is interrupted, save the best model obtained so far
+    except KeyboardInterrupt:
+        print('Training interrupted')
+        if save_model:
+            print('Saving model before exiting...')
+            print(f'With validation loss: {min_valid_loss}')
+            model.load_state_dict(best_model_weights)
+            meta_data = {
+                'learning_rate': learning_rate,
+                'epochs': epochs,
+                'loss_function_name': loss_function_name,
+                'optimizer_name': optimizer_name,
+                'history': history,
+            }
+            model_utils.save_model_and_meta_data(
+                model=model,
+                custom_transforms=custom_transforms,
+                save_path=save_path,
+                meta_data=meta_data,
+                verbose=verbose,
+            )
+        raise
 
     # save model (weights and configuration) and other useful information
-    meta_data = {
-        'learning_rate': learning_rate,
-        'epochs': epochs,
-        'loss_function_name': loss_function_name,
-        'optimizer_name': optimizer_name,
-        'history': history,
-    }
     if save_model:
+        meta_data = {
+            'learning_rate': learning_rate,
+            'epochs': epochs,
+            'loss_function_name': loss_function_name,
+            'optimizer_name': optimizer_name,
+            'history': history,
+        }
         model_utils.save_model_and_meta_data(
             model=model,
             custom_transforms=custom_transforms,
