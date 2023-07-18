3bf1d5283514d1ddaa0413ca89fb98acd803ea9d
Marco Conciatori <conciatori.marco91@gmail.com>
2023-07-18 10:07:18 +0900

    updated script for parameter search

--- best_parameters_grid_search.py
+++ best_parameters_grid_search.py
@@ -47,127 +47,128 @@ utils.pretty_print_dict(data=search_space)
 print(f'num_tests_for_configuration: {num_tests_for_configuration}')
 
 results = []
-try:
-    for model_spec in search_space['model_spec_list']:
-        model_name, weights_name = model_spec
-        print(f'model: {model_name}, with weights: {weights_name}')
-        custom_transforms, resize_in_attributes = model_utils.get_custom_transforms(
-            weights_name=weights_name,
-            verbose=verbose,
-        )
+# try:
+print('\nStarting grid search...')
+for model_spec in search_space['model_spec_list']:
+    model_name, weights_name = model_spec
+    print(f'model: {model_name}, with weights: {weights_name}')
+    custom_transforms, resize_in_attributes = model_utils.get_custom_transforms(
+        weights_name=weights_name,
+        verbose=verbose,
+    )
 
-        for batch_size in search_space['batch_size_list']:
-            start_time = datetime.datetime.now()
-            print(f'\tbatch_size: {batch_size}')
-            for balance_data in search_space['balance_data_list']:
-                print(f'\t\tbalance_data: {balance_data}')
-                for data_augmentation_proportion in search_space['data_augmentation_proportion_list']:
-                    print(f'\t\t\tdata_augmentation_proportion: {data_augmentation_proportion}')
-                    train_dl, val_dl, test_dl, img_shape = get_ready_data.get_data(
-                        batch_size=batch_size,
-                        shuffle=config.SHUFFLE,
-                        balance_data=balance_data,
-                        custom_transforms=custom_transforms,
-                        train_val_test_proportions=config.TRAIN_VAL_TEST_PROPORTIONS,
-                        no_resizing=resize_in_attributes,
-                        tolerance=config.TOLERANCE,
-                        augmentation_proportion=data_augmentation_proportion,
-                        random_seed=config.RANDOM_SEED,
-                        verbose=verbose,
-                    )
-                    end_time = datetime.datetime.now()
-                    print(f'\tdata processing/loading time: {utils.timedelta_format(start_time, end_time)}')
-                    start_time = end_time
+    for batch_size in search_space['batch_size_list']:
+        start_time = datetime.datetime.now()
+        print(f'\tbatch_size: {batch_size}')
+        for balance_data in search_space['balance_data_list']:
+            print(f'\t\tbalance_data: {balance_data}')
+            for data_augmentation_proportion in search_space['data_augmentation_proportion_list']:
+                print(f'\t\t\tdata_augmentation_proportion: {data_augmentation_proportion}')
+                train_dl, val_dl, test_dl, img_shape = get_ready_data.get_data(
+                    batch_size=batch_size,
+                    shuffle=config.SHUFFLE,
+                    balance_data=balance_data,
+                    custom_transforms=custom_transforms,
+                    train_val_test_proportions=config.TRAIN_VAL_TEST_PROPORTIONS,
+                    no_resizing=resize_in_attributes,
+                    tolerance=config.TOLERANCE,
+                    augmentation_proportion=data_augmentation_proportion,
+                    random_seed=config.RANDOM_SEED,
+                    verbose=verbose,
+                )
+                end_time = datetime.datetime.now()
+                print(f'\t\t\tdata processing/loading time: {utils.timedelta_format(start_time, end_time)}')
+                start_time = end_time
 
-                    for optimizer_name in search_space['optimizer_name_list']:
-                        print(f'\t\t\t\toptimizer: {optimizer_name}')
-                        for learning_rate in search_space['learning_rate_list']:
-                            print(f'\t\t\t\t\tlearning_rate: {learning_rate}')
-                            for num_epochs in search_space['num_epochs_list']:
-                                print(f'\t\t\t\t\t\tnum_epochs: {num_epochs}')
+                for optimizer_name in search_space['optimizer_name_list']:
+                    print(f'\t\t\t\toptimizer: {optimizer_name}')
+                    for learning_rate in search_space['learning_rate_list']:
+                        print(f'\t\t\t\t\tlearning_rate: {learning_rate}')
+                        for num_epochs in search_space['num_epochs_list']:
+                            print(f'\t\t\t\t\t\tnum_epochs: {num_epochs}')
 
-                                configuration_loss_test = 0
-                                configuration_loss_test_no_nan = 0
-                                no_nan_counter = 0
-                                for i in range(num_tests_for_configuration):
-                                    model = model_utils.get_torchvision_model(
-                                        model_name=model_name,
-                                        weights_name=weights_name,
-                                        device=device,
-                                        training=True,
-                                        num_classes=num_classes,
-                                    )
-
-                                    training_history = training.train(
-                                        model=model,
-                                        training_data=train_dl,
-                                        validation_data=val_dl,
-                                        num_epochs=num_epochs,
-                                        learning_rate=learning_rate,
-                                        loss_function_name=config.LOSS_FUNCTION_NAME,
-                                        optimizer_name=optimizer_name,
-                                        device=device,
-                                        verbose=verbose,
-                                        save_model=False,
-                                        custom_transforms=custom_transforms,
-                                    )
-
-                                    test_loss, metric_evaluations = evaluation.eval(
-                                        model=model,
-                                        test_data=test_dl,
-                                        loss_function_name=config.LOSS_FUNCTION_NAME,
-                                        device=device,
-                                        display_confusion_matrix=False,
-                                        metrics=config.METRICS,
-                                        save_results=False,
-                                        verbose=verbose,
-                                    )
-                                    configuration_loss_test += test_loss
-                                    is_nan = False
-                                    no_nan_counter += 1
-                                    for metric_name in metric_evaluations:
-                                        metric_result = metric_evaluations[metric_name]
-                                        if np.isnan(metric_result):
-                                            is_nan = True
-                                            no_nan_counter -= 1
-                                            break
-
-                                    if not is_nan:
-                                        configuration_loss_test_no_nan += test_loss
-
-                                configuration_loss_test = configuration_loss_test / num_tests_for_configuration
-                                if no_nan_counter > 0:
-                                    configuration_loss_test_no_nan = configuration_loss_test_no_nan / no_nan_counter
-                                else:
-                                    configuration_loss_test_no_nan = 99.
-
-                                print(f'\t\t\t\t\t\t\tconfiguration_loss_test: {configuration_loss_test}')
-                                print(f'\t\t\t\t\t\t\tconfiguration_loss_test_no_nan: {configuration_loss_test_no_nan}')
-                                results.append(
-                                    {
-                                        'test_loss': configuration_loss_test,
-                                        'test_loss_no_nan': configuration_loss_test_no_nan,
-                                        'no_nan_counter': no_nan_counter,
-                                        'learning_rate': learning_rate,
-                                        'balance_data': balance_data,
-                                        'batch_size': batch_size,
-                                        'data_augmentation_proportion': data_augmentation_proportion,
-                                        'optimizer_name': optimizer_name,
-                                        'num_epochs': num_epochs,
-                                        'model': (model_name, weights_name),
-                                    },
+                            configuration_loss_test = 0
+                            configuration_loss_test_no_nan = 0
+                            no_nan_counter = 0
+                            for i in range(num_tests_for_configuration):
+                                model = model_utils.get_torchvision_model(
+                                    model_name=model_name,
+                                    weights_name=weights_name,
+                                    device=device,
+                                    training=True,
+                                    num_classes=num_classes,
                                 )
-                                end_time = datetime.datetime.now()
-                                print(f'\t\t\t\t\t\t\t{num_tests_for_configuration} identical models trained and tested'
-                                      f' in: {utils.timedelta_format(start_time, end_time)}')
-                                start_time = end_time
 
-except Exception as e:
-    print(e)
-    interrupted = True
-    end_time = datetime.datetime.now()
-    print(f'Computation interrupted after: {utils.timedelta_format(global_start_time, end_time)}')
-    print('Saving partial results...')
+                                training_history = training.train(
+                                    model=model,
+                                    training_data=train_dl,
+                                    validation_data=val_dl,
+                                    num_epochs=num_epochs,
+                                    learning_rate=learning_rate,
+                                    loss_function_name=config.LOSS_FUNCTION_NAME,
+                                    optimizer_name=optimizer_name,
+                                    device=device,
+                                    verbose=verbose,
+                                    save_model=False,
+                                    custom_transforms=custom_transforms,
+                                )
+
+                                test_loss, metric_evaluations = evaluation.eval(
+                                    model=model,
+                                    test_data=test_dl,
+                                    loss_function_name=config.LOSS_FUNCTION_NAME,
+                                    device=device,
+                                    display_confusion_matrix=False,
+                                    metrics=config.METRICS,
+                                    save_results=False,
+                                    verbose=verbose,
+                                )
+                                configuration_loss_test += test_loss
+                                is_nan = False
+                                no_nan_counter += 1
+                                for metric_name in metric_evaluations:
+                                    metric_result = metric_evaluations[metric_name]
+                                    if np.isnan(metric_result):
+                                        is_nan = True
+                                        no_nan_counter -= 1
+                                        break
+
+                                if not is_nan:
+                                    configuration_loss_test_no_nan += test_loss
+
+                            configuration_loss_test = configuration_loss_test / num_tests_for_configuration
+                            if no_nan_counter > 0:
+                                configuration_loss_test_no_nan = configuration_loss_test_no_nan / no_nan_counter
+                            else:
+                                configuration_loss_test_no_nan = 99.
+
+                            print(f'\t\t\t\t\t\t\tconfiguration_loss_test: {configuration_loss_test}')
+                            print(f'\t\t\t\t\t\t\tconfiguration_loss_test_no_nan: {configuration_loss_test_no_nan}')
+                            results.append(
+                                {
+                                    'test_loss': configuration_loss_test,
+                                    'test_loss_no_nan': configuration_loss_test_no_nan,
+                                    'no_nan_counter': no_nan_counter,
+                                    'learning_rate': learning_rate,
+                                    'balance_data': balance_data,
+                                    'batch_size': batch_size,
+                                    'data_augmentation_proportion': data_augmentation_proportion,
+                                    'optimizer_name': optimizer_name,
+                                    'num_epochs': num_epochs,
+                                    'model': (model_name, weights_name),
+                                },
+                            )
+                            end_time = datetime.datetime.now()
+                            print(f'\t\t\t\t\t\t\t{num_tests_for_configuration} identical models trained and tested'
+                                  f' in: {utils.timedelta_format(start_time, end_time)}')
+                            start_time = end_time
+
+# except Exception as e:
+#     print(e)
+#     interrupted = True
+#     end_time = datetime.datetime.now()
+#     print(f'Computation interrupted after: {utils.timedelta_format(global_start_time, end_time)}')
+#     print('Saving partial results...')
 
 print('Test ended')
 print('Results:')
