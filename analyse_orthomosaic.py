8b9d35a1ca44abc876b26c9e0e0790c59edf24ae
Marco Conciatori <conciatori.marco91@gmail.com>
2023-07-21 16:08:09 +0900

    updated analyse_orthomosaic

--- analyse_orthomosaic.py
+++ analyse_orthomosaic.py
@@ -1,16 +1,16 @@
 import cv2
-import warnings
+import torch
+import numpy as np
 import tifffile as tifi
 import torchvision.transforms.functional as tf
 
 import utils
-import config
 import global_constants
 from models import model_utils
-from data_preprocessing import image_utils
+from image_processing import orthomosaic_utils
 
 
-# compute species probability distribution for each pixel in the orthomosaic image
+# compute species probability distribution for each pixel in the image_processing image
 
 # PARAMETERS
 verbose = 2
@@ -19,9 +19,11 @@ partial_name = 'DEFAULT'
 img_name = 'Zao1_211005.tif'
 # in pixels
 # set patch_size to None to use the crop_size from the model. Only works for torchvision pretrained models
-patch_size = None
 # TODO: chiedere a quanti pixel corrisponde una patch reale di 2m x 2m
+patch_size = None
 stride = 128
+# confidence prediction probability above which the prediction is considered valid
+confidence_threshold = 0.6
 
 # load model
 device = utils.get_available_device(verbose=verbose)
@@ -41,18 +43,29 @@ if custom_transforms is not None:
     preprocess = custom_transforms[1]
     if patch_size is None:
         patch_size = preprocess.crop_size[0]
-    print(f'patch_size from preprocess: {patch_size}')
+    # print(f'patch_size from preprocess: {patch_size}')
 else:
     assert patch_size is not None, 'patch_size must be specified if no custom_transforms are used'
     preprocess = None
-print(f'preprocess: {preprocess}')
+print(f'network patch_size: {patch_size}')
 
 # load orthomosaic image
 orthomosaic_path = global_constants.ORTHOMOSAIC_DATA_PATH + img_name
 orthomosaic = tifi.imread(orthomosaic_path)
 print(f'orthomosaic.shape: {orthomosaic.shape}')
+# print(f'orthomosaic type: {type(orthomosaic)}')
+# print(f'orthomosaic[0, 0, 0] type: {type(orthomosaic[0, 0, 0])}')
 total_width = orthomosaic.shape[1]
 total_height = orthomosaic.shape[0]
+x_max = total_width - patch_size
+y_max = total_height - patch_size
+
+unknown_class_id = meta_data['num_classes']
+
+# initialize species distribution
+# the last channel is used to count the number of times a pixel has been predicted
+# the second to last channel is used to count the number of times a pixel has been predicted as unknown
+species_distribution = np.zeros((total_height, total_width, meta_data['num_classes'] + 2), dtype=np.int8)
 
 # remove fourth channel
 orthomosaic = orthomosaic[:, :, 0:3]
@@ -66,21 +79,63 @@ print(f'change color space orthomosaic.shape: {orthomosaic.shape}')
 orthomosaic = tf.to_tensor(orthomosaic)
 print(f'to tensor orthomosaic.shape: {orthomosaic.shape}')
 
+# # to device
+# image_processing = image_processing.to(device)
+# print(f'to device {device}, image_processing type: {type(image_processing)}')
+
+softmax = torch.nn.Softmax(dim=0)
 # to limit the amount of ram used, one patch at a time is extracted from the orthomosaic image and fed to the model
-for x in range(0, total_width, stride):
-    for y in range(0, total_height, stride):
+for x in range(0, x_max, stride):
+    for y in range(0, y_max, stride):
         print(f'x: {x}, y: {y}')
         # extract patch
-        patch = image_utils.get_patch(img=orthomosaic, size=patch_size, top_left_coord=(x, y))
+        patch = orthomosaic_utils.get_patch(img=orthomosaic, size=patch_size, top_left_coord=(x, y))
         print(f'original patch.shape: {patch.shape}')
-        # cv2.imshow('patch', patch)
-        # cv2.waitKey(0)
-        # cv2.destroyAllWindows()
 
         # apply preprocessing
         if preprocess is not None:
             patch = preprocess(patch)
-            print(f'transform patch.shape: {patch.shape}')
+            print(f'transformed patch.shape: {patch.shape}')
+        # cv2.imshow('patch', patch)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
 
+        # to device
+        patch = patch.to(device)
+
+        single_patch_batch = patch.unsqueeze(0)
+        print(f'single_patch_batch.shape: {single_patch_batch.shape}')
+        single_prediction_batch = loaded_model(single_patch_batch)
+        print(f'single_prediction_batch.shape: {single_prediction_batch.shape}')
+        prediction = single_prediction_batch.squeeze(0)
+        print(f'prediction.shape: {prediction.shape}')
+        print(f'prediction: {prediction}')
+        prediction = softmax(prediction)
+        print(f'prediction softmax: {prediction}')
+        prediction = prediction.detach().cpu().numpy()
+        print(f'prediction.detach().cpu().numpy(): {prediction}')
+        predicted_class = prediction.argmax()
+        print(f'predicted_class: {predicted_class}')
+        predicted_probability = prediction[predicted_class]
+        if predicted_probability < confidence_threshold:
+            predicted_class = unknown_class_id
+
+        print('species_distribution[y: y + patch_size, x: x + patch_size, : ] shape: ')
+        print(f'{species_distribution[y: y + patch_size, x: x + patch_size, : ].shape}')
+        print(f'{species_distribution[y: y + 5, x: x + 5, : ]}')
+        species_distribution[
+            y:y + patch_size,
+            x:x + patch_size,
+            predicted_class,
+        ] += 1
+        species_distribution[
+            y:y + patch_size,
+            x:x + patch_size,
+            -1,
+        ] += 1
+
+
+        print('species_distribution[y: y + patch_size, x: x + patch_size, : ] shape: ')
+        print(f'{species_distribution[y: y + patch_size, x: x + patch_size, : ].shape}')
+        print(f'{species_distribution[y: y + 5, x: x + 5, : ]}')
         exit()
-
