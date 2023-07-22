de262137c28d3ca6ec9df222881dba01ded38f07
Marco Conciatori <conciatori.marco91@gmail.com>
2023-07-22 12:12:28 +0900

    updated analyse_orthomosaic

--- analyse_orthomosaic.py
+++ analyse_orthomosaic.py
@@ -2,6 +2,7 @@ import cv2
 import torch
 import numpy as np
 import tifffile as tifi
+from pathlib import Path
 import torchvision.transforms.functional as tf
 
 import utils
@@ -24,6 +25,7 @@ patch_size = None
 stride = 128
 # confidence prediction probability above which the prediction is considered valid
 confidence_threshold = 0.6
+img_name_no_extension = img_name[ : -4]
 
 # load model
 device = utils.get_available_device(verbose=verbose)
@@ -52,6 +54,8 @@ print(f'network patch_size: {patch_size}')
 # load orthomosaic image
 orthomosaic_path = global_constants.ORTHOMOSAIC_DATA_PATH + img_name
 orthomosaic = tifi.imread(orthomosaic_path)
+# TODO: remove this line, only for speeding up testing
+orthomosaic = orthomosaic[10000 : 12000, 10000 : 12000, :]
 print(f'orthomosaic.shape: {orthomosaic.shape}')
 # print(f'orthomosaic type: {type(orthomosaic)}')
 # print(f'orthomosaic[0, 0, 0] type: {type(orthomosaic[0, 0, 0])}')
@@ -61,19 +65,22 @@ x_max = total_width - patch_size
 y_max = total_height - patch_size
 
 unknown_class_id = meta_data['num_classes']
+num_classes_plus_unknown = meta_data['num_classes'] + 1
 
 # initialize species distribution
 # the last channel is used to count the number of times a pixel has been predicted
 # the second to last channel is used to count the number of times a pixel has been predicted as unknown
-species_distribution = np.zeros((total_height, total_width, meta_data['num_classes'] + 2), dtype=np.int8)
+species_distribution = np.zeros((total_height, total_width, num_classes_plus_unknown + 1), dtype=np.int8)
 
 # remove fourth channel
 orthomosaic = orthomosaic[:, :, 0:3]
 print(f'remove fourth channel orthomosaic.shape: {orthomosaic.shape}')
 
 # change color space
-orthomosaic = cv2.cvtColor(orthomosaic, cv2.COLOR_BGR2RGB)
-print(f'change color space orthomosaic.shape: {orthomosaic.shape}')
+# TODO: check if this is needed, probably it is not needed because probably the model is
+#  trained with images without this conversion
+# orthomosaic = cv2.cvtColor(orthomosaic, cv2.COLOR_BGR2RGB)
+# print(f'change color space orthomosaic.shape: {orthomosaic.shape}')
 
 # to tensor
 orthomosaic = tf.to_tensor(orthomosaic)
@@ -87,15 +94,15 @@ softmax = torch.nn.Softmax(dim=0)
 # to limit the amount of ram used, one patch at a time is extracted from the orthomosaic image and fed to the model
 for x in range(0, x_max, stride):
     for y in range(0, y_max, stride):
-        print(f'x: {x}, y: {y}')
+        # print(f'x: {x}, y: {y}')
         # extract patch
         patch = orthomosaic_utils.get_patch(img=orthomosaic, size=patch_size, top_left_coord=(x, y))
-        print(f'original patch.shape: {patch.shape}')
+        # print(f'original patch.shape: {patch.shape}')
 
         # apply preprocessing
         if preprocess is not None:
             patch = preprocess(patch)
-            print(f'transformed patch.shape: {patch.shape}')
+            # print(f'transformed patch.shape: {patch.shape}')
         # cv2.imshow('patch', patch)
         # cv2.waitKey(0)
         # cv2.destroyAllWindows()
@@ -104,38 +111,76 @@ for x in range(0, x_max, stride):
         patch = patch.to(device)
 
         single_patch_batch = patch.unsqueeze(0)
-        print(f'single_patch_batch.shape: {single_patch_batch.shape}')
+        # print(f'single_patch_batch.shape: {single_patch_batch.shape}')
         single_prediction_batch = loaded_model(single_patch_batch)
-        print(f'single_prediction_batch.shape: {single_prediction_batch.shape}')
+        # print(f'single_prediction_batch.shape: {single_prediction_batch.shape}')
         prediction = single_prediction_batch.squeeze(0)
-        print(f'prediction.shape: {prediction.shape}')
-        print(f'prediction: {prediction}')
+        # print(f'prediction.shape: {prediction.shape}')
+        # print(f'prediction: {prediction}')
         prediction = softmax(prediction)
-        print(f'prediction softmax: {prediction}')
+        # print(f'prediction softmax: {prediction}')
         prediction = prediction.detach().cpu().numpy()
-        print(f'prediction.detach().cpu().numpy(): {prediction}')
+        # print(f'prediction.detach().cpu().numpy(): {prediction}')
         predicted_class = prediction.argmax()
-        print(f'predicted_class: {predicted_class}')
+        # print(f'predicted_class: {predicted_class}')
         predicted_probability = prediction[predicted_class]
+        # print(f'predicted_probability: {predicted_probability}')
         if predicted_probability < confidence_threshold:
             predicted_class = unknown_class_id
+        # print(f'predicted_class unknown?: {predicted_class}')
 
-        print('species_distribution[y: y + patch_size, x: x + patch_size, : ] shape: ')
-        print(f'{species_distribution[y: y + patch_size, x: x + patch_size, : ].shape}')
-        print(f'{species_distribution[y: y + 5, x: x + 5, : ]}')
+        # print(f'species_distribution[{x} : {x + patch_size}, {y} : {y + patch_size}, : ]: '
+        #       f'{species_distribution[x : x + patch_size, y : y + patch_size, : ].shape}')
+        # print(f'{species_distribution[x: x + 5, y: y + 5, : ].shape}')
+        # print(f'{species_distribution[x: x + 5, y: y + 5, : ]}')
         species_distribution[
-            y:y + patch_size,
-            x:x + patch_size,
+            x : x + patch_size,
+            y : y + patch_size,
             predicted_class,
         ] += 1
         species_distribution[
-            y:y + patch_size,
             x:x + patch_size,
+            y:y + patch_size,
             -1,
         ] += 1
+        # print('species_distribution[x: x + patch_size, y: y + patch_size, : ] shape: ')
+        # print(f'{species_distribution[x: x + patch_size, y: y + patch_size, : ].shape}')
+        # print(f'{species_distribution[x: x + 5, y: y + 5, : ]}')
 
+print(f'species_distribution.shape: {species_distribution.shape}')
+print(f'species_distribution max: {species_distribution.max()}')
+print(f'species_distribution min: {species_distribution.min()}')
+# normalize species_distribution using the information in the last channel
+for x in range(0, x_max, stride):
+    for y in range(0, y_max, stride):
+        for c in range(num_classes_plus_unknown):
+            species_distribution[x, y, c] = species_distribution[x, y, c] / species_distribution[x, y, -1]
+print(f'species_distribution.shape: {species_distribution.shape}')
+print(f'species_distribution max: {species_distribution.max()}')
+print(f'species_distribution min: {species_distribution.min()}')
 
-        print('species_distribution[y: y + patch_size, x: x + patch_size, : ] shape: ')
-        print(f'{species_distribution[y: y + patch_size, x: x + patch_size, : ].shape}')
-        print(f'{species_distribution[y: y + 5, x: x + 5, : ]}')
-        exit()
+# remove last channel
+species_distribution = species_distribution[:, :, 0 : num_classes_plus_unknown]
+print(f'species_distribution.shape: {species_distribution.shape}')
+
+# create one image for each class, where the alpha channel is the probability of that pixel being that class
+save_path = f'{global_constants.OUTPUT_DIR}{global_constants.ORTHOMOSAIC_FOLDER_NAME}{img_name_no_extension}/'
+Path(save_path).mkdir(parents=True, exist_ok=True)
+for c in range(num_classes_plus_unknown):
+    temp_img = np.zeros((total_height, total_width, 4), dtype=np.uint8)
+    temp_img[:, :, 3] = species_distribution[:, :, c] * 255
+    if c == unknown_class_id:
+        colors = (0, 0, 0)
+    else:
+        colors = global_constants.TREE_INFORMATION[c]['display_color_rgb']
+    temp_img[:, :, 0] = colors[0]
+    temp_img[:, :, 1] = colors[1]
+    temp_img[:, :, 2] = colors[2]
+    # cv2.imshow(f'{img_name_no_extension}_{c}', temp_img)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+    cv2.imwrite(
+        f'{save_path}{img_name_no_extension}_{c}.png',
+        temp_img,
+    )
+
