af40343dd99cd06e326a886cd64f6fa8e6456c70
Marco Conciatori <conciatori.marco91@gmail.com>
2023-09-29 11:54:48 +0200

    test notebook display

--- show_confusion_matrix.py
+++ show_confusion_matrix.py
@@ -5,7 +5,7 @@ import global_constants
 from visualization import visualization_utils
 
 
-def show_cm():
+def show_confusion_matrix():
     # parameters
     model_id = int(input('Insert model id number: '))
     partial_name = str(input('Insert name or part of the name to distinguish between models with the same id number: '))
@@ -25,3 +25,7 @@ def show_cm():
     print(f'len prediction_list: {len(prediction_list)}')
     # Plot the confusion matrix
     visualization_utils.display_cm(true_values=tag_list, predictions=prediction_list, save_img=False)
+
+
+if __name__ == '__main__':
+    show_confusion_matrix()
