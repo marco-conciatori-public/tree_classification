fb31faced231fbb0b316386791343275d8987129
Marco Conciatori <conciatori.marco91@gmail.com>
2023-10-06 18:25:17 +0200

    fix: not a keyword argument

--- biodiversity_metrics/shannon_wiener_index.py
+++ biodiversity_metrics/shannon_wiener_index.py
@@ -12,7 +12,7 @@ def get_bio_diversity_index(tag_list: list, log_base=2):
     shannon_wiener_index = 0
     for tree in proportion_of_trees_by_species:
         p_i = proportion_of_trees_by_species[tree]
-        shannon_wiener_index += p_i * math.log(p_i, base=log_base)
+        shannon_wiener_index += p_i * math.log(p_i, log_base)
 
     shannon_wiener_index *= -1
     return shannon_wiener_index
