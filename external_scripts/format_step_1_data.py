850e4041144b57b63869c5c9b5d852b9783f41f0
Marco <marco.conciatori@unipr.it>
2023-11-30 16:42:22 +0100

    removed debug prints

--- external_scripts/format_step_1_data.py
+++ external_scripts/format_step_1_data.py
@@ -13,19 +13,19 @@ species_counter = {}
 
 for file_path in pure_path.iterdir():
     if file_path.is_file():
-        print(f'file_path: {file_path}')
+        # print(f'file_path: {file_path}')
         file_name = file_path.name
-        print(f'file_name: {file_name}')
+        # print(f'file_name: {file_name}')
         new_file_name = file_name.lower()
-        print(f'new_file_name: {new_file_name}')
+        # print(f'new_file_name: {new_file_name}')
         if new_file_name.startswith('nana_'):
             new_file_name = new_file_name.replace('nana_', 'nanakamado_')
-            print(f'new_file_name: {new_file_name}')
+            # print(f'new_file_name: {new_file_name}')
         new_file_path = f'{file_path.parent}\\{new_file_name}'
-        print(f'new_file_path: {new_file_path}')
+        # print(f'new_file_path: {new_file_path}')
         file_path.rename(new_file_path)
         file_name = new_file_name
-        print(f'actual file_path: {file_path}')
+        # print(f'actual file_path: {file_path}')
 
         species_name = file_name.split('_')[0]
         if species_name not in species_counter:
