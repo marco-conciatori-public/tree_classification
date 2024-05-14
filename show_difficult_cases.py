036e243d725f27933cb85e8f514edc717b423c77
Marco Conciatori <conciatori.marco91@gmail.com>
2024-05-14 16:17:40 +0200

    refactoring

--- show_difficult_cases.py
+++ show_difficult_cases.py
@@ -6,7 +6,7 @@ import utils
 import global_constants
 from import_args import args
 from models import model_utils
-from data_preprocessing import get_ready_data, data_loading
+from data_preprocessing import data_loading
 
 
 def show_difficult_cases_(**kwargs):
